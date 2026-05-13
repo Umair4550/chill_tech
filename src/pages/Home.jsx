e89ce490e14409fadc369d0de9268ce505bbdca1
umair4550 <muhammadumair55708@gmail.com>
2026-05-14 00:09:48 +0500

    after adding the add unit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,27 @@ import ServicesCarousel from "../components/ServicesCarousel";
 import ServicesSection from "../components/ServicesSection";
 import Testimonials from "../components/Testimonials";
 import AboutSection from "../components/AboutSection";
-import ContactSection from "../components/ContactSection"; // Add this import
+import ContactSection from "../components/ContactSection";
 
 const Home = () => {
   return (
     <>
       {/* HERO / HOME SECTION */}
       <section id="home">
+        {/* Banner Ad */}
+        <div style={{ marginBottom: '30px' }}>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4894327526032532"
+               crossorigin="anonymous"></script>
+          <ins className="adsbygoogle"
+               style={{ display: 'block' }}
+               data-ad-client="ca-pub-4894327526032532"
+               data-ad-slot="7971448803"
+               data-ad-format="auto"
+               data-full-width-responsive="true"></ins>
+          <script dangerouslySetInnerHTML={{
+            __html: "(adsbygoogle = window.adsbygoogle || []).push({});"
+          }} />
+        </div>
         <ServicesCarousel />
       </section>
 
@@ -25,8 +39,23 @@ const Home = () => {
         <AboutSection />
       </section>
 
+      {/* Ad before Contact */}
+      <div style={{ margin: '30px 0' }}>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4894327526032532"
+             crossorigin="anonymous"></script>
+        <ins className="adsbygoogle"
+             style={{ display: 'block' }}
+             data-ad-client="ca-pub-4894327526032532"
+             data-ad-slot="5786920003"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script dangerouslySetInnerHTML={{
+          __html: "(adsbygoogle = window.adsbygoogle || []).push({});"
+        }} />
+      </div>
+
       {/* CONTACT SECTION */}
-      <ContactSection /> {/* Add this line */}
+      <ContactSection />
     </>
   );
 };
